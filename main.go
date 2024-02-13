0077228cb74f66d3fa6b47940c759bd4e77d3def
Yoon Sung Ji <ajys@umich.edu>
2024-02-13 13:23:16 -0500

    save progress

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
+import "net"
+
+func handleConnection(conn net.Conn) {
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Print(err)
+		}
+	}(conn)
+
+}
 
 func main() {
-	fmt.Println("ping pong server running...")
+	fmt.Println("Starting Ping Pong Application...")
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		// Write "pong" back to the client
-		_, err := fmt.Fprintf(w, "pong\n")
-		if err != nil {
-			return
-		}
-	})
-
-	fmt.Println("Server is running on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	ln, err := net.Listen("tcp", "127.0.0.1:1080")
 	if err != nil {
-		return
+		fmt.Print(err)
+	}
+	// TODO: might have to defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Print(err)
+		}
+		go handleConnection(conn)
 	}
 }
